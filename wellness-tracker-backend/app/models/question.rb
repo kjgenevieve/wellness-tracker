a286e3a8cdc04f72c5fe4f2ff002b732f552cfa2
github username <github email address>
2019-10-02 11:10:15 -0700

    Restart

--- wellness-tracker-backend/app/models/question.rb
+++
@@ -1,4 +0,0 @@
-class Question < ApplicationRecord
-  has_many :answers
-  has_many :users, through: :answers
-end
--- wellness-tracker-backend/app/models/user.rb
+++
@@ -1,4 +0,0 @@
-class User < ApplicationRecord
-  has_many :answers
-  has_many :questions, through: :answers
-end
