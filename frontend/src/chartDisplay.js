5acdb789dce544739bc043c6064e2318eafa421c
github username <github email address>
2019-10-04 10:03:39 -0700

    Refactored code

--- frontend/src/chartDisplay.js
+++ frontend/src/chartDisplay.js
@@ -1,6 +1,69 @@
-document.addEventListener("DOMContentLoaded", () => {
-    displayChart();
-})
+// document.addEventListener("DOMContentLoaded", () => {
+//     displayChart();
+// })
+
+function prepForChart(data, user) {
+    const mainEl = document.getElementById("main")
+    if (mainEl.hasChildNodes()) {
+        mainEl.innerHTML = "";
+    };
+    
+    const chartEl = document.getElementById("chart")
+    if (chartEl.hasChildNodes()) {
+        mainEl.innerHTML = "";
+    };
+    
+    const myChart = document.createElement("canvas")
+    myChart.id = "myChart"
+    myChart.width = "400"
+    myChart.height = "400"
+    chartEl.appendChild(myChart)
+    
+    dashboardListeners(user, mainEl);
+    displayChart(data);
+};
+
+function displayChart(data) {
+    // console.log(readyData)
+    var ctx = document.getElementById('myChart');
+    var myChart = new Chart(ctx, {
+        type: 'polarArea',
+        data: {
+            labels: createLabels(),
+            datasets: [{
+                label: 'Responses',
+                data: prepareData(data),
+                backgroundColor: function(context) {
+                    var indexBG = context.dataIndex;
+                    return indexBG < 7 ? 'rgba(255, 99, 132, 0.2)' :
+                        indexBG < 13 ? 'rgba(54, 162, 235, 0.2)' :
+                        indexBG < 19 ? 'rgba(255, 206, 86, 0.2)' :
+                        indexBG < 25 ? 'rgba(75, 192, 192, 0.2)' :
+                        indexBG < 31 ? 'rgba(153, 102, 255, 0.2)' :
+                        'rgba(255, 159, 64, 0.2)';
+                },
+                borderColor: function(context) {
+                    var indexB = context.dataIndex;
+                    return indexB < 7 ? 'rgba(255, 99, 132, 1)' :
+                        indexB < 13 ? 'rgba(54, 162, 235, 1)' :
+                        indexB < 19 ? 'rgba(255, 206, 86, 1)' :
+                        indexB < 25 ? 'rgba(75, 192, 192, 1)' :
+                        indexB < 31 ? 'rgba(153, 102, 255, 1)' :
+                        'rgba(255, 159, 64, 1)';
+                },
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                display: true
+            },
+            legend: {
+                display: false
+            }
+        }
+    })
+};
 
 function createLabels() {
     let labelsAry = []
@@ -23,47 +86,12 @@ function createLabels() {
         labelsAry.push(`Spiritual Q${i}`)
     }
     return labelsAry
-}
+};
 
-function displayChart(){
-    var ctx = document.getElementById('myChart');
-    var myChart = new Chart(ctx, {
-        type: 'polarArea',
-        data: {
-            labels: createLabels(),
-            datasets: [{
-                label: '# of Votes',
-                data: [10, 80, 50, 20, 70, 100, 10, 80, 50, 20, 70, 100, 10, 80, 50, 20, 70, 100, 10, 80, 50, 20, 70, 100, 10, 80, 50, 20, 70, 100, 10, 80, 50, 20, 70, 100],
-                backgroundColor: function(context) {
-                    var index = context.dataIndex;
-                    var value = context.dataset.data[index];
-                    return index < 7 ? 'rgba(255, 99, 132, 0.2)' :
-                        index < 13 ? 'rgba(54, 162, 235, 0.2)' :
-                        index < 19 ? 'rgba(255, 206, 86, 0.2)' :
-                        index < 25 ? 'rgba(75, 192, 192, 0.2)' :
-                        index < 31 ? 'rgba(153, 102, 255, 0.2)' :
-                        'rgba(255, 159, 64, 0.2)';
-                },
-                borderColor: function(context) {
-                    var index = context.dataIndex;
-                    var value = context.dataset.data[index];
-                    return index < 7 ? 'rgba(255, 99, 132, 1)' :
-                        index < 13 ? 'rgba(54, 162, 235, 1)' :
-                        index < 19 ? 'rgba(255, 206, 86, 1)' :
-                        index < 25 ? 'rgba(75, 192, 192, 1)' :
-                        index < 31 ? 'rgba(153, 102, 255, 1)' :
-                        'rgba(255, 159, 64, 1)';
-                },
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                display: true
-            },
-            legend: {
-                display: false
-            }
-        }
-    });
+function prepareData(data) {
+    let readyData = []
+    for (i = 1; i <= 36; i++) {
+        readyData.push(parseInt(data[i].response, 10));
+    };
+    return readyData
 }
---
+++ frontend/src/dashboard.js
@@ -0,0 +1,30 @@
+function loadDashboard(user) {
+    const mainEl = document.getElementById("main")
+    if (mainEl.hasChildNodes()) {
+        mainEl.innerHTML = "";
+    };
+
+    const chartEl = document.getElementById("chart")
+    if (chartEl.hasChildNodes()) {
+        chartEl.innerHTML = "";
+    };
+    
+    const welcome = document.createElement("p")
+    welcome.textContent = `Hello, ${user.name}!`
+    mainEl.appendChild(welcome)
+
+    // More info will be displayed here later
+
+    // Only show "Take New Test" button if user is logged in
+    dashboardListeners(user, mainEl);
+};
+
+function dashboardListeners(user, mainEl) {
+    const newTestBtn = document.getElementById("newTestBtn");
+    newTestBtn.addEventListener("click", () => prepNewTest(user, mainEl));
+
+    const dashboardBtn = document.getElementById("dashboard");
+    dashboardBtn.addEventListener("click", () => loadDashboard(user));
+
+    // Other listeners go here.
+}
